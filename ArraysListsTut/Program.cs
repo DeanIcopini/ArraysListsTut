809a6b431784f68a07f504e724beec9b6de95c19
DeanIcopini <40065319+DeanIcopini@users.noreply.github.com>
2018-08-02 15:12:53 -0700

    Add files via upload

---
+++ ArraysListsTut/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ArraysListsTut
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Arrays are used primarily when there is a fixed number of items that you are working with (like items that took place in the past, maybe within a designated time frame). Or if you are storing a very large quantity of items i.e. bytes in a picture.
+            // Lists are used when the data items you are working with have the potential to change.
+
+            // LIST EXAMPLES
+
+            //List<int> intList = new List<int>();
+            // To change data type:
+            List<string> intList = new List<string>();
+            intList.Add("4");
+            intList.Add("10");
+            intList.Remove("10");
+
+            Console.WriteLine(intList[0]);
+            Console.Read();
+
+            // ARRAY EXAMPLES
+
+            //int[] numArray = new int[5];
+            //numArray[0] = 5;
+            //numArray[1] = 2;
+            //numArray[2] = 10;
+            //numArray[3] = 200;
+            //numArray[4] = 5000;
+
+            //// The above code can also be written like:
+            //int[] numArray1 = new int[] { 5, 2, 10, 200, 5000};
+
+            //// A faster way
+            //int[] numArray2 = { 2, 5, 10, 200, 5000, 620, 2300 };
+
+            //// To change an item in an array
+            //numArray2[5] = 650;
+            
+            //Console.WriteLine(numArray1[2]);
+            //Console.Read();
+        }
+    }
+}
---
+++ ArraysListsTut/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ArraysListsTut")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ArraysListsTut")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("435e298d-223c-4737-87a6-cd24ee5c3d4b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
